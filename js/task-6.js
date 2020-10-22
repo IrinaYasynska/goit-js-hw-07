f5ed189e1901a20e5de5d1b33841b8a0e521d343
Iryna Yasynska <irina159159@gmail.com>
2020-10-22 22:58:37 +0300

    6

--- js/task-6.js
+++ js/task-6.js
@@ -3,11 +3,11 @@ const inputValue = document.querySelector(`#validation-input`);
 inputValue.addEventListener('blur', validationAmountSymbol);
 
 function validationAmountSymbol() {
-    if (inputValue.value.length == inputValueEl.dataset.length) {
+    if (inputValue.value.length == inputValue.dataset.length) {
         inputValue.classList.add("valid");
         inputValue.classList.remove("invalid");
     }
-    if (inputValue.value.length != inputValueEl.dataset.length) {
+    if (inputValue.value.length != inputValue.dataset.length) {
         inputValue.classList.add("invalid");
         inputValue.classList.remove("valid");
     }
