e87b35bbea1bc6388ff9aa626865d14283eca9ba
Iryna Yasynska <irina159159@gmail.com>
2020-10-26 12:02:17 +0200

    Number

--- js/task-4.js
+++ js/task-4.js
@@ -3,10 +3,12 @@ const incrementBtn = document.querySelector('[data-action="increment"]');
 const counterValue = document.querySelector('#value');
 let currentValue = 0;
 const increment = () => {
-  return (Number(counterValue.textContent) = currentValue += 1);
+  return Number(counterValue.textContent = currentValue += 1);
 };
 const decrement = () => {
-  return (Number(counterValue.textContent) = currentValue -= 1);
+  return Number(counterValue.textContent = currentValue -= 1);
 };
 decrementBtn.addEventListener('click', decrement);
 incrementBtn.addEventListener('click', increment);
+
+console.log(typeof counterValue.textContent ); 
